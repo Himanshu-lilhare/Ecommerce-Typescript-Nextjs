414f606b92432663275caf295b2c26edc3251810
Himanshu Lilhare <rajlilhare200@gmail.com>
2023-08-25 22:37:18 +0530

    fix navbar

--- packages/ui/navbar/FirstLinks.tsx
+++ packages/ui/navbar/FirstLinks.tsx
@@ -1,19 +1,40 @@
-"use client"
+"use client";
 import Link from "next/link";
-import { useRecoilValue } from 'recoil';
-import { menuSelector } from 'store';
+import { useCallback, useEffect } from "react";
+import { useRecoilState } from "recoil";
+import { menuAtom } from "store";
 const FirstLinks = () => {
-    const menu = useRecoilValue(menuSelector)
+  const [menu, setMenu] = useRecoilState(menuAtom);
+
+  const closeNavbar = useCallback(() => {
+    setMenu(false);
+  }, [setMenu]);
+
+  useEffect(() => {
+    const closeNavbarOnClickOutside = (e: any) => {
+      if (menu && !e.target.closest(".first-links")) {
+        closeNavbar();
+      }
+    };
+
+    document.addEventListener("click", closeNavbarOnClickOutside);
+
+    return () => {
+      document.removeEventListener("click", closeNavbarOnClickOutside);
+    };
+  }, [menu, closeNavbar]);
   return (
     <div
-    className={
-      menu ? "first-links first-links-d-flex" : "first-links-d-none"
-    }
-  >
-    <Link href={"/"}>Home</Link>
-    <Link href={"/shop"}>Shop</Link>
-  </div>
-  )
-}
+      className={menu ? "first-links first-links-d-flex" : "first-links-d-none"}
+    >
+      <Link onClick={() => closeNavbar()} href={"/"}>
+        Home
+      </Link>
+      <Link onClick={() => closeNavbar()} href={"/shop"}>
+        Shop
+      </Link>
+    </div>
+  );
+};
 
-export default FirstLinks
+export default FirstLinks;
