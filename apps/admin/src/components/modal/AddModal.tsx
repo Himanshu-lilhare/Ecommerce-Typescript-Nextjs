e7c221d26ac15062a4dc6f46d081776ec25a353a
Himanshu Lilhare <rajlilhare200@gmail.com>
2023-09-09 17:23:26 +0530

    modal done

--- apps/admin/src/components/modal/AddModal.tsx
+++ apps/admin/src/components/modal/AddModal.tsx
@@ -1,68 +1,84 @@
-import { Dispatch, SetStateAction } from "react"
-import {useForm} from "react-hook-form"
+import { Dispatch, SetStateAction } from "react";
+import { useForm } from "react-hook-form";
+import "./addmodal.scss";
 type Props = {
-    slug:string,
-    columns:number[],
-    setOpen: Dispatch<SetStateAction<boolean>>
-}
+  slug: string;
+  columns: number[];
+  setOpen: Dispatch<SetStateAction<boolean>>;
+};
 type signupform = {
-    email:string,
-    password:string,
-    name:string
-}
-const AddModal = (props:Props) => {
-
-    const form = useForm<signupform>();
-    const { handleSubmit, formState } = form;
-    console.log(formState)
-    function submitForm(data:signupform){
-console.log(data)
-
-    }
+  email: string;
+  password: string;
+  name: string;
+};
+const AddModal = (props: Props) => {
+  const form = useForm<signupform>();
+  const { handleSubmit, formState } = form;
+  console.log(formState);
+  function submitForm(data: signupform) {
+    console.log(data);
+  }
   return (
     <div className="add-modal">
-<div className="modal">
-<span className="close" onClick={()=>props.setOpen(false)}>
-    X
-</span>
-<h2>Add New {props.slug}</h2>
-<form onSubmit={handleSubmit(submitForm)}  noValidate>
+      <div className="modal">
+        <span className="close" onClick={() => props.setOpen(false)}>
+          x
+        </span>
+        <h2>Add New {props.slug}</h2>
+        <form onSubmit={handleSubmit(submitForm)} noValidate>
+          <div className="item">
+            <label htmlFor="email">Email</label>
+            <input
+              type="mail"
+              placeholder="email"
+              {...form.register("email", {
+                required: "Email is Required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Please Provide Email in Right Format",
+                },
+              })}
+            />
+            <p>{formState.errors?.email?.message}</p>
+          </div>
+          <div className="item">
+          <label htmlFor="name">Name</label>
+            <input
+              type="name"
+              placeholder="name"
+              {...form.register("name", {
+                required: "Name is Required",
+                minLength: {
+                  value: 3,
+                  message: "min Length is 3",
+                },
+              })}
+            />
+            <p>{formState.errors?.name?.message}</p>
+          </div>
+          <div className="item">
+          <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              placeholder="password"
+              {...form.register("password", {
+                required: "Password is Required",
+                pattern: {
+                  value:
+                    /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/,
+                  message:
+                    "Password Must Contain Special Character , Number and Uppercase Letter",
+                },
+              })}
+            />
 
-    <input type="mail" placeholder="email" {...form.register("email",{
-        required:"Email is Required",
-        pattern: {
-            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-            message: "Please Provide Email in Right Format",
-          },
-    })}  />
-        <p>{formState.errors?.email?.message}</p>
-
-     <input type="name" placeholder="password" {...form.register("name",{
-        required:"Name is Required",
-         minLength:{
-            value:3,
-            message:'min Length is 3'
-         }
-    })}  />
-        <p>{formState.errors?.name?.message}</p>
-       <input type="password" placeholder="password" {...form.register("password",{
-        required:"Password is Required",
-        pattern: {
-            value:
-              /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]+$/,
-            message:
-              "Password Must Contain Special Character , Number and Uppercase Letter",
-          },
-    })}  />
-        <p>{formState.errors?.password?.message}</p>
-   
-    
-</form>
-
-</div>
-
+            <p>{formState.errors?.password?.message}</p>
+          </div>
+          <button>Create User</button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddModal
+export default AddModal;
